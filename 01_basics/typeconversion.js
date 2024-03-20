62c24a529b1e9deb05dcace1b31027208bf0884e
10sudikshA <113459600+10sudikshA@users.noreply.github.com>
2024-03-20 17:11:34 +0000

    typeconver

---
+++ 01_basics/typeconversion.js
@@ -0,0 +1,8 @@
+let str = "33";
+let num = Number(str);
+// let n = NaN;
+// let na = Number(n);
+console.log(typeof(str));
+console.log(typeof(num));
+console.log(typeof(n));
+console.log(NaN===NaN);
