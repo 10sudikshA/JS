06fe3214b17f33410cb75ac64fd721f7e6814791
10sudikshA <113459600+10sudikshA@users.noreply.github.com>
2024-03-20 16:46:38 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,15 @@
+"use strict";//by writing this in code ,it specifies that all the code is written in newer version of javascript
+//alert("hi"); ....this will not work as we are using node.js and alret have a different syntax here
+console.log(3+1);
+// let a=3+"3";
+console.log(3+"3");
+console.log("a");
+console.log(true+false);
+
+// Number
+// BigInt
+// Boolean
+// String
+// undefined : its value has not been intialzed yet ,undefined is of type undefined
+// null : it is empty does not have any value null is of type object
+
