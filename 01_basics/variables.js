405a81889d25519163eb9528c7de2029ae34797b
10sudikshA <113459600+10sudikshA@users.noreply.github.com>
2024-03-20 14:36:29 +0000

    learned var

---
+++ 01_basics/variables.js
@@ -0,0 +1,19 @@
+
+let c=1;
+
+{
+    //  let accountId=11;
+    var a=2;
+    const accountId = 10;
+    console.log(accountId);
+    c++;
+    console.log(c);
+    c=19;
+    console.log(c);
+    a=78;
+    console.log(a);
+
+}
+// console.log(accountId);
+console.log(c);
+console.log(a);
