32095c85e4825596fcdaacc959f731cd1386ee4d
10sudikshA <113459600+10sudikshA@users.noreply.github.com>
2024-04-16 15:47:10 +0000

    op

--- 01_basics/operations.js
+++ 01_basics/operations.js
@@ -1,1 +1,6 @@
-console.log("1"+"2");      
+// console.log("1"+"2");      
+// console.log(2+1+"2");  
+// console.log("2"+2+1);  
+// console.log(false+true);
+// console.log(true<3>4);
+// console.log(+"");
