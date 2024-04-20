c91b08aa6f65cccb1bb0d4ad9ea51847c03a78cb
10sudikshA <113459600+10sudikshA@users.noreply.github.com>
2024-04-20 11:08:01 +0000

    modified

---
+++ 01_basics/strings.js
@@ -0,0 +1,11 @@
+let str  = "hello";
+let str2  = "            hello       ";
+let ob1 = new String("world");
+console.log(ob1.length);
+//String interpolation
+console.log(`Welcome guys this is ${str} ${ob1} in string`);
+console.log(str.toUpperCase());
+console.log(str2.trim());
+const url = "this~is~url~for~testing";
+console.log(typeof url.split("~"));
+
