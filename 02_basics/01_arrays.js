59ca092d285b7b1728fad55245d131030d2919ce
10sudikshA <113459600+10sudikshA@users.noreply.github.com>
2024-04-20 12:33:04 +0000

    arr1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,19 @@
+//array
+let newarr = [1,2,3,4,5];
+console.log(newarr);
+let arr2 = new Array("s","u","d","i",10);
+console.log(arr2[0]);
+//array methods
+arr2.push("a");
+console.log(arr2);
+console.log(arr2.pop());
+console.log(arr2);
+arr2.unshift(15);
+console.log(arr2);
+arr2.shift();
+console.log(arr2);
+console.log(arr2.includes(10));
+console.log(arr2.indexOf('s'));
+console.log( arr2.join());
+console.log(arr2.slice(0,2));
+console.log(arr2.splice(2,3));
