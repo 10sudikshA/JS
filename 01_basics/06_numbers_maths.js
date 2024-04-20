72e4154a2458721e4c5106749e970c4a9589df6a
10sudikshA <113459600+10sudikshA@users.noreply.github.com>
2024-04-20 11:37:42 +0000

    math and num

---
+++ 01_basics/06_numbers_maths.js
@@ -0,0 +1,26 @@
+const sc = 400;
+// console.log(sc);
+
+// const newSc = new Number(1000.00000);
+// console.log(newSc);
+// console.log(newSc.toString().charAt(0));
+
+// console.log(newSc.toFixed(2));//trim to decimal places
+// const newNum = 12.476;
+// console.log(newNum.toPrecision(4));
+// console.log(newNum.toPrecision(2));//round off
+
+const hund = 1000000;
+// console.log(hund.toLocaleString());//give commas between according to international standard 
+// console.log(hund.toLocaleString('en-IN'));//commas according to indian standard
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_VALUE);
+
+
+//=====================================================================================================
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+const min = 10;
+const max = 20 ; 
+console.log(Math.floor((Math.random()*(max-min+1)+min)));
